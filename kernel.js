b3d1fae5f5174410fa695f2bd3571e8c33e99955
THE WOLF HACK <70449764+wolf-5@users.noreply.github.com>
2022-01-18 19:21:08 +0200

    672a

--- kernel.js
+++ kernel.js
@@ -56868,11 +56868,7 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
-if (main_ret == 179 || main_ret == 0){
-ldbarx.className = "Ani2";
-}
-else {
-localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
-loaderx.style.display="none";failx.style.display="block";return;
-}
-   }
+if(main_ret==179||main_ret==0)
+{window.msgs.innerHTML="<h1 style='font-size:30px;text-align:center;'>jailbreak 100/100 OK ✔</h1>";} 
+else
+{window.msgs.innerHTML="<h1 style='font-size:30px;text-align:center;'>Jailbreak failed! - Reboot your PS4 and try again.</h1>";}}
